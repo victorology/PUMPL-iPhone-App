7e1b3f9cad24d7de6a112b31f34480034a4fc494
Sergey Nikitenko <sergeyn@finalview.com>
2011-06-26 22:30:49 +0300

    Updatin ImagePicker: adding semitransparent button and setting X image to close button

--- Classes/ImagePicker/PLImagePickerController.m
+++ Classes/ImagePicker/PLImagePickerController.m
@@ -2,7 +2,7 @@
 //  PLImagePickerController.m
 //  
 //
-//  Created by Sergey Nikitenko on 5/8/11.
+//  Created by Sergey Nikitenko on 6/25/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
@@ -23,8 +23,6 @@
     self.titleLabel.shadowOffset = prototypeButton.titleLabel.shadowOffset;
 }
 @end
-
-
 
 
 @implementation PLImagePickerController
@@ -64,19 +62,31 @@
         UIButton* cancelButton = [[pickerBar subviews] objectAtIndex:1];
         if([cancelButton isKindOfClass:[UIButton class]])
         {
+            /*
             UIButton* squareButton = [UIButton buttonWithType:UIButtonTypeCustom];
             squareButton.frame = CGRectOffset(cancelButton.frame, pickerBar.frame.size.width-2*cancelButton.frame.origin.x-cancelButton.frame.size.width, 0);
             [squareButton configureAsPrototypeButton:cancelButton];
             [squareButton setTitle:@"Square" forState:UIControlStateNormal];
             [squareButton addTarget:self action:@selector(onSquareBtn:) forControlEvents:UIControlEventTouchUpInside];
             [pickerBar addSubview:squareButton];
+            */ 
             
-            
-            UIImage* squareImage = [UIImage imageNamed:@"croppedCameraOverlay.png"];
+            UIImage* squareImage = [UIImage imageNamed:@"SquareView.png"];
             squareView = [[[UIImageView alloc] initWithImage:squareImage] autorelease];
             squareView.frame = CGRectMake(0, 0, 320, 427);
             squareView.hidden = YES;
             [[[[pickerBar superview] superview] superview] addSubview:squareView];
+            
+            UIButton* squareBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+            squareBtn.frame = CGRectMake(20, 10, 83, 35);
+            [squareBtn setImage:[UIImage imageNamed:@"Square.png"] forState:UIControlStateNormal];
+            [squareBtn addTarget:self action:@selector(onSquareBtn:) forControlEvents:UIControlEventTouchUpInside];
+            [[[[[pickerBar superview] superview] superview] superview] addSubview:squareBtn];
+            
+            
+            [cancelButton setImage:[UIImage imageNamed:@"Close.png"] forState:UIControlStateNormal];
+            cancelButton.frame = CGRectMake(cancelButton.frame.origin.x+10, cancelButton.frame.origin.y, cancelButton.frame.size.height, cancelButton.frame.size.height);
+            
             return YES;
         }
     }
@@ -98,17 +108,18 @@
     }
 }
 
- 
 -(void) onSquareBtn:(id)sender
 {
-    if([[sender titleForState:UIControlStateNormal] isEqualToString:@"Square"])
+    if(squareView.hidden)
     {
-        [sender setTitle:@"Full" forState:UIControlStateNormal];
+        [sender setImage:[UIImage imageNamed:@"Full.png"] forState:UIControlStateNormal];
+        //[sender setTitle:@"Full" forState:UIControlStateNormal];
         squareView.hidden = NO;
     }
     else
     {
-        [sender setTitle:@"Square" forState:UIControlStateNormal];
+        [sender setImage:[UIImage imageNamed:@"Square.png"] forState:UIControlStateNormal];
+        //[sender setTitle:@"Square" forState:UIControlStateNormal];
         squareView.hidden = YES;
     }
 }
